0a7f601c17a78a50974b51c89c4b59dae4db6289
David Wang <david.wang@kirinou.top>
2022-07-06 15:19:15 +0800

    wip(member): 🚧 TODO: 调整成员数据库字段

--- src/main/java/org/netunion/manager/controller/MemberController.java
+++ src/main/java/org/netunion/manager/controller/MemberController.java
@@ -3,7 +3,6 @@ package org.netunion.manager.controller;
 import org.netunion.manager.config.Member;
 import org.netunion.manager.mapper.MemberMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
