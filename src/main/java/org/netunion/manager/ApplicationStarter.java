3db8bba7a2cfa399cc3cd3f4734bf4331fbc7806
David Wang <david.wang@kirinou.top>
2022-07-07 21:16:29 +0800

    wip(login): 🚧 重构认证模块

--- src/main/java/org/netunion/manager/ApplicationStarter.java
+++ src/main/java/org/netunion/manager/ApplicationStarter.java
@@ -2,6 +2,7 @@ package org.netunion.manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 /**
  * 启动程序
--- src/main/java/org/netunion/manager/config/SecurityConfig.java
+++ src/main/java/org/netunion/manager/config/SecurityConfig.java
@@ -1,2 +1,56 @@
-package org.netunion.manager.config;public class SecurityConfig {
+package org.netunion.manager.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig{
+    @Bean
+    public UserDetailsService users() {
+        // The builder will ensure the passwords are encoded before saving in memory
+        User.UserBuilder users = User.withDefaultPasswordEncoder();
+        UserDetails user = users
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+        UserDetails admin = users
+                .username("admin")
+                .password("password")
+                .roles("USER", "ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+//    @Bean
+//    public WebSecurityCustomizer webSecurityCustomizer() {
+//        return (web) -> web.ignoring().antMatchers("/member");
+//    }
+    @Bean
+    public SecurityFilterChain httpSecurity(HttpSecurity http) throws Exception {
+//        http.authorizeHttpRequests((authz) -> authz
+//                .anyRequest().authenticated())
+//                .formLogin().and()
+//                .csrf().disable();
+//        return http.build();
+        ExpressionUrlAuthorizationConfigurer<HttpSecurity>.ExpressionInterceptUrlRegistry registry = http.authorizeRequests();
+        registry.antMatchers("/member").permitAll();
+        registry.anyRequest().authenticated();
+        registry.and().formLogin().and().csrf().disable();
+        return http.build();
+    }
 }
