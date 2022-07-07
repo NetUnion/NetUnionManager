8540cf022f1c451e4ad34d094156f2ce25e287d2
David Wang <david.wang@kirinou.top>
2022-07-07 21:14:47 +0800

    wip(login): 🚧 重构认证模块

--- src/main/java/org/netunion/manager/config/SecurityConfig.java
+++ src/main/java/org/netunion/manager/config/SecurityConfig.java
@@ -1,24 +1,2 @@
-package org.netunion.manager.config;
-
-import org.netunion.manager.interceptor.JwtInterceptor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-/**
- * JWT 拦截器使能
- *
- * @author David Wang
- * @date 2020-07-07
- * @version 1.0
- */
-@Configuration
-public class SecurityConfig implements WebMvcConfigurer {
-    @Autowired
-    private JwtInterceptor jwtInterceptor;
-
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(jwtInterceptor).addPathPatterns("/**").excludePathPatterns("/login", "/login/*"); //所有接口必须检查JWT
-    }
+package org.netunion.manager.config;public class SecurityConfig {
 }
