613c812a182d0a91f20f099c15a8a62e56fff595
David Wang <david.wang@kirinou.top>
2022-07-08 10:24:10 +0800

    style(security): 🎨 优化import

--- src/main/java/org/netunion/manager/config/SecurityConfig.java
+++ src/main/java/org/netunion/manager/config/SecurityConfig.java
@@ -2,22 +2,13 @@ package org.netunion.manager.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -38,10 +29,6 @@ public class SecurityConfig{
                 .build();
         return new InMemoryUserDetailsManager(user, admin);
     }
-//    @Bean
-//    public WebSecurityCustomizer webSecurityCustomizer() {
-//        return (web) -> web.ignoring().antMatchers("/member");
-//    }
     @Bean
     public SecurityFilterChain httpSecurity(HttpSecurity http) throws Exception {
         http
