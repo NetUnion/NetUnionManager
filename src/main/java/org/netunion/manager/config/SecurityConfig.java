f38adecc2342620ea77d599d0a3825633fc86647
David Wang <david.wang@kirinou.top>
2022-07-11 15:06:00 +0800

    fix(security): 🐛 优化import

--- src/main/java/org/netunion/manager/config/SecurityConfig.java
+++ src/main/java/org/netunion/manager/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package org.netunion.manager.config;
 
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
